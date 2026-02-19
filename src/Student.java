adfbd7fbc4efd85b4d31f90ffc519e60b4760af7
karriem-lang <karriemlang@proton.me>
2026-02-18 22:54:16 -0500

    Initial Commit: UML and Requirments/Deliverables...

---
+++ src/Student.java
@@ -0,0 +1,45 @@
+/*
+Understanding UML
+
+Student -> Class name
+
+PRIVATE variables -> '-' means private
+- firstName: String
+- lastName: String
+- gpa: double ->
+
+CONSTRUCTOR (creates objects)  -> '+' means public
++ Student(firstName: String, lastName: String, gpa: double)
+
+GETTER Methods (access data)
++ getFirstName(): String
++ getLastName(): String
++ getGPA(): double
+
+SETTER Methods (modify data)
++ setGPA(gpa: double):
+
+SPECIAL method (string rep)
++ toString(): String
+*/
+//============================================================//
+/*
+Requirements:
+START:
+    Ask: Number of students
+    Loop:
+        for each student
+            first name
+            last name
+            gpa -> if gpa < 0 -> assign value 0
+            create object
+            sore object in array
+    Exit Loop:
+        Calculate avg gpa -> format to two places
+DONE
+*/
+
+public class Student {
+
+    
+}
