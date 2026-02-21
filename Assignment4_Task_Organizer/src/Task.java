8ecab3cbbb5dfe1540deb34227d232c8bf7d3db7
karriem-lang <karriemlang@proton.me>
2026-02-20 22:11:45 -0500

    Base classes written - not tested - v1.0

--- Assignment4_Task_Organizer/src/Task.java
+++ Assignment4_Task_Organizer/src/Task.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.text.DecimalFormat;
 
+/*
+ Base -> Parent -> Super Class
+ */
 public class Task {
+    // Private (access specifiers) -> Encapsulation
+    private String name;
+    private double duration;
+    private String priorityLevel;
+
+    /*
+     Constructor: The Handshake Protocol
+     Constructs a new Task w/ specified Attributes
+     @param name -> Task name
+     @param duration -> Task duration
+     @param priorityLevel -> Task priority
+    */
+    public Task(String name, double duration, String priorityLevel) {
+        this.name = name;
+        this.duration = duration;
+        this.priorityLevel = priorityLevel.toLowerCase(); // bonus: normalize input
+    }
+
+    // accessor method -> Getter
+    public double getDuration() {
+        return duration;
+    }
+
+    /*
+    Helper printInfo() built to be overridden [no FINAL]
+    allowing subclasses liberty in print method implementation
+    */
+    public void printInfo() {
+        // Format duration to 2 decimal places
+        DecimalFormat df = new DecimalFormat("0.00");
+        // Python like -> "print formatted" %s -> String %n -> newline
+        System.out.printf("Task: %s%n", name);
+        // alternate format from the java.text.DecimalFormat
+        System.out.println("Duration: " + df.format(duration) + " hours");
+        System.out.println("Priority Level: " + priorityLevel);
+    }
 }
+
