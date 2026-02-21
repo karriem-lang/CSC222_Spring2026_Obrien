63846d58970c3e52908b7fd06e5a0359b89df7c6
karriem-lang <karriemlang@proton.me>
2026-02-20 23:19:14 -0500

    SchoolTask Complete..

--- Assignment4_Task_Organizer/src/SchoolTask.java
+++ Assignment4_Task_Organizer/src/SchoolTask.java
@@ -1,2 +1,27 @@
-public class SchoolTask {
+/**
+ OOP in action!
+ The Inheritance pillar of OPP allows us to extend an existing class (Task)
+ This new class HomeTask inherits the members of the class it extends -> Task
+ in addition to its own unique attributes -> meetingCount and deadLine
+ */
+
+public class SchoolTask extends Task {
+
+    private int meetingCount; // SchoolTask -> number of meetings
+    private String deadline;  // SchoolTask -> deadline
+
+    // Constructor calls the base class constructor -> see Constructor comment in {HomeTask}
+    public SchoolTask(String name, double duration, String priorityLevel, int meetingCount, String deadline) {
+        super(name, duration, priorityLevel); 
+        this.meetingCount = meetingCount;
+        this.deadline = deadline;
+    }
+
+    // Override printInfo to include school-specific details
+    @Override
+    public void printInfo() {
+        super.printInfo(); // print base task info
+        System.out.println("Number of Meetings: " + meetingCount);
+        System.out.println("Deadline: " + deadline);
+    }
 }
